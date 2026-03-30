8e502bb7e794dd9721b826d211e6e4f1c81f477d
Klipar <bhdnkarpenko123@gmail.com>
2026-03-30 22:36:36 +0200

    add creating new core request

--- agent/src/handler/new_core_handler.rs
+++ agent/src/handler/new_core_handler.rs
@@ -1,12 +1,14 @@
 use async_trait::async_trait;
 use serde_json::{Value, json};
-use shared::server::{connection_context::ConnectionContext, handler_trait::HandlerTrait, message::{Message, Status}};
+use shared::{db::models::Core, server::{connection_context::ConnectionContext,
+                                        dto::create_core_dto::CreateCoreDto,
+                                        generate_token::generate_token,
+                                        handler_trait::HandlerTrait,
+                                        message::{Message, Status}}};
 use sqlx::postgres::PgPool;
 use std::sync::Arc;
 
-use rand::RngCore;
-use base64::{engine::general_purpose, Engine as _};
-use sha2::{Sha256, Digest};
+use log::{info, error};
 
 pub struct NewCoreHandler {
     pub pool: Arc<PgPool>,
@@ -20,30 +22,57 @@ impl NewCoreHandler {
 
 #[async_trait]
 impl HandlerTrait for NewCoreHandler {
-    async fn handle(&self, data: Value, ctx: &mut ConnectionContext) -> Message {
-        println!("Creating new core using data: {}", data);
+    async fn handle(&self, data: Value, _ctx: &mut ConnectionContext) -> Message {
+        info!("Creating new core using data: {}", data);
 
-        let mut bytes = [0u8; 32];
-        rand::thread_rng().fill_bytes(&mut bytes);
+        let dto: CreateCoreDto = match serde_json::from_value(data) {
+            Ok(v) => v,
+            Err(e) => {
+                error!("Failed to parse create new core request: {}", e);
+                return Message::new_response(
+                    Status::Error,
+                    json!({ "message": "Invalid new-core request" }),
+                    400,
+                );
+            }
+        };
 
-        // encode in Base64
-        let token = general_purpose::STANDARD.encode(&bytes);
+        let (token, hash) = generate_token();
 
-        println!("{}", token);
+        let inserted = sqlx::query_as!(
+            Core,
+            r#"
+            INSERT INTO cores (ip, port, name, token_hash)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id, ip, port, name, token_hash
+            "#,
+            dto.ip,
+            dto.port,
+            dto.name,
+            hash
+        )
+        .fetch_one(&*self.pool)
+        .await;
 
+        match inserted {
+            Ok(core) => {
+                info!("Successful created new core: `{}`", core.name);
 
-        let mut hasher = Sha256::new();
-        hasher.update(token.as_bytes());
-        let result = hasher.finalize();
+                return Message::new_response (
+                    Status::Ok,
+                    json!({"message":"Created successfully!", "token":token}),
+                    200,
+                );
+            }
+            Err(e) => {
+                error!("Failed to create core: {}", e);
 
-        // encode hash in Base64
-        let hash_base64 = general_purpose::STANDARD.encode(result);
-
-        println!("{}", hash_base64);
-        return Message::new_response (
-            Status::Error,
-            json!({ "message": "Not implemented" }),
-            9999,
-        );
+                return Message::new_response (
+                    Status::Error,
+                    json!({"message":"Failed to create new core."}),
+                    500,
+                );
+            }
+        }
     }
 }
--- shared/src/server/mod.rs
+++ shared/src/server/mod.rs
@@ -2,3 +2,5 @@ pub mod server;
 pub mod handler_trait;
 pub mod connection_context;
 pub mod message;
+pub mod dto;
+pub mod generate_token;
