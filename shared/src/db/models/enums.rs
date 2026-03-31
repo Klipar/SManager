8e320169694f26cbe8a8d560458b011af06bf6d6
Klipar <bhdnkarpenko123@gmail.com>
2026-03-31 19:51:43 +0200

    fixed migration, add new-task request

--- shared/src/db/models/enums.rs
+++ shared/src/db/models/enums.rs
@@ -3,12 +3,15 @@ use sqlx::Type;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Type)]
 #[sqlx(type_name = "restart_policy", rename_all = "lowercase")]
+#[serde(rename_all = "lowercase")]
 pub enum RestartPolicy {
     No,
     Always,
+
+    #[serde(rename = "on-failure")]
+    #[sqlx(rename = "on-failure")]
     OnFailure,
 }
-
 #[derive(Debug, Clone, Serialize, Deserialize, Type)]
 #[sqlx(type_name = "task_status", rename_all = "lowercase")]
 pub enum TaskStatus {
--- shared/src/server/dto/mod.rs
+++ shared/src/server/dto/mod.rs
@@ -1,2 +1,3 @@
 pub mod create_core_dto;
 pub mod get_cores_dto;
+pub mod new_task_request_dto;
