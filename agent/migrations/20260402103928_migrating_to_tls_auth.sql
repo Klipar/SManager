5799708ac65c3d2ca1654d7c0602e6461c1b6274
Klipar <bhdnkarpenko123@gmail.com>
2026-04-02 13:47:50 +0200

    add base test core to db

--- agent/migrations/20260402103928_migrating_to_tls_auth.sql
+++ agent/migrations/20260402103928_migrating_to_tls_auth.sql
@@ -6,3 +6,7 @@ RENAME COLUMN token_hash TO client_hash;
 DROP INDEX IF EXISTS idx_cores_token_hash;
 
 CREATE UNIQUE INDEX idx_cores_client_hash ON CORES(client_hash);
+
+-- add test core
+INSERT INTO cores (ip, name, client_hash)
+    VALUES ('127.0.0.1', 'testing core', 'lI/mA/YdwDa1xZbcCf484/PTDckPAkyF88gtssyrZ50=');
