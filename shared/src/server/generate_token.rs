821312108d6e0db576103be1a0985609f7e2406b
Klipar <bhdnkarpenko123@gmail.com>
2026-04-04 12:25:21 +0200

    changed generate_token func behavior.

--- shared/src/server/generate_token.rs
+++ shared/src/server/generate_token.rs
@@ -1,15 +1,9 @@
 use rand::RngCore;
 use base64::{engine::general_purpose, Engine as _};
 
-use crate::server::get_hash::get_hash;
-
-pub fn generate_token() -> (String, String) {
+pub fn generate_token() -> String {
     let mut bytes = [0u8; 32];
     rand::thread_rng().fill_bytes(&mut bytes);
 
-    let token = general_purpose::STANDARD.encode(&bytes);
-
-    let token_hash = get_hash(&token);
-
-    (token, token_hash)
+    general_purpose::STANDARD.encode(&bytes)
 }
